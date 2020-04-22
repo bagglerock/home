65856126a96e516dff9dfc80b9a6ccb69be9f872
Oscar Villalta <bagglerock@gmail.com>
2020-04-22 01:37:27 -0400

    feat: add some content

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,5 +1,29 @@
 import React from "react";
 
 export const Footer: React.FC = () => (
-  <div className="footer">This is the footer section</div>
+  <>
+    <h4>&copy Oscar Villalta 2019</h4>
+    <ul className="social-links">
+      <li>
+        <a href="https://github.com/bagglerock" target="_blank">
+          <i className="fab fa-github-square fa-2x"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/oscar-villalta/" target="_blank">
+          <i className="fab fa-linkedin fa-2x"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.hackerrank.com/oscarvillaltanj" target="_blank">
+          <i className="fab fa-hackerrank fa-2x"></i>
+        </a>
+      </li>
+      <li>
+        <a href="mailto:oscarvillaltanj@gmail.com" target="_blank">
+          <i className="fas fa-envelope-square fa-2x"></i>
+        </a>
+      </li>
+    </ul>
+  </>
 );
