af3bb0d9f860562ab054129b8435a4a31fe51ea7
Oscar Villalta <bagglerock@gmail.com>
2020-05-04 01:43:26 -0400

    fix: add noopener noreferrer to anchor tags

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -5,22 +5,38 @@ export const Footer: React.FC = () => (
     <h4>&copy Oscar Villalta 2019</h4>
     <ul className="social-links">
       <li>
-        <a href="https://github.com/bagglerock" target="_blank">
+        <a
+          href="https://github.com/bagglerock"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-github-square fa-2x"></i>
         </a>
       </li>
       <li>
-        <a href="https://www.linkedin.com/in/oscar-villalta/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/oscar-villalta/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-linkedin fa-2x"></i>
         </a>
       </li>
       <li>
-        <a href="https://www.hackerrank.com/oscarvillaltanj" target="_blank">
+        <a
+          href="https://www.hackerrank.com/oscarvillaltanj"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-hackerrank fa-2x"></i>
         </a>
       </li>
       <li>
-        <a href="mailto:oscarvillaltanj@gmail.com" target="_blank">
+        <a
+          href="mailto:oscarvillaltanj@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fas fa-envelope-square fa-2x"></i>
         </a>
       </li>
