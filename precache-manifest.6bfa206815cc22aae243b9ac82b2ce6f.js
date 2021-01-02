52f109a5a4f65cf91ad3ef84513cba61716db582
bagglerock <bagglerock@gmail.com>
2021-01-01 23:03:42 -0500

    Updates

---
+++ precache-manifest.6bfa206815cc22aae243b9ac82b2ce6f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c59207d39355bb7336ff6db35276cc",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "fe2246da9f9b3797809e",
+    "url": "/home/static/css/main.8642391f.chunk.css"
+  },
+  {
+    "revision": "9c16741753a925bdc777",
+    "url": "/home/static/js/2.9fc6297b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/home/static/js/2.9fc6297b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2246da9f9b3797809e",
+    "url": "/home/static/js/main.46ec3e66.chunk.js"
+  },
+  {
+    "revision": "b03c9ad13c32bc2337c4",
+    "url": "/home/static/js/runtime-main.b24d4e3d.js"
+  },
+  {
+    "revision": "ea9d371c2a40e41b1b712e91d5e2cc85",
+    "url": "/home/static/media/Material Memories.ea9d371c.jpg"
+  },
+  {
+    "revision": "9cf80c360bd069f3691621fc5758b0c9",
+    "url": "/home/static/media/background.9cf80c36.jpeg"
+  },
+  {
+    "revision": "11e41b2f8843e69934396bd6ac15506d",
+    "url": "/home/static/media/chefsden.11e41b2f.jpg"
+  },
+  {
+    "revision": "ac2ba3e57533db5749299823e69301fe",
+    "url": "/home/static/media/daves_logo_lighter.ac2ba3e5.png"
+  },
+  {
+    "revision": "a532f777244646af1acca7ed2a3134e1",
+    "url": "/home/static/media/hangman.a532f777.jpg"
+  },
+  {
+    "revision": "74a510e9bffae3496155ed7b8d81fc6f",
+    "url": "/home/static/media/jam.74a510e9.png"
+  },
+  {
+    "revision": "25738e49c9a60562f25af03ef95252df",
+    "url": "/home/static/media/knockout.25738e49.jpg"
+  },
+  {
+    "revision": "4b21b1e1267c67848998ea9a63841a75",
+    "url": "/home/static/media/kris-01.4b21b1e1.png"
+  },
+  {
+    "revision": "0bb88e0d9093bda9d0843e638913ab17",
+    "url": "/home/static/media/peachco.0bb88e0d.gif"
+  },
+  {
+    "revision": "c23adbf02e91d86c5fcb09698764ed11",
+    "url": "/home/static/media/rhe.c23adbf0.png"
+  },
+  {
+    "revision": "fb8ff3350a0450817daecb22d032f811",
+    "url": "/home/static/media/rps.fb8ff335.jpg"
+  },
+  {
+    "revision": "870a1985d22d4b23dc84bec732fabf3a",
+    "url": "/home/static/media/trivia.870a1985.png"
+  }
+]);
--- src/sections/Contact/Contact.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Contact: React.FC = () => (
-  <div className="contact">
-    <button>
-      <a href="mailto:oscarvillaltanj@gmail.com">Contact Me</a>
-    </button>
-  </div>
-);
